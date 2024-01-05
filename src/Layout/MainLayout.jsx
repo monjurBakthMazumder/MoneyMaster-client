4ec9f39492a3b54cbf27bac254643218e909a65e
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-05 20:53:12 +0600

    update

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Component/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
     </div>
   );
